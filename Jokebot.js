e7117a62172e5621f05e922bed1eb60cff306efb
corbenboyd <77169439+corbenboyd@users.noreply.github.com>
2021-05-07 13:56:28 -0400

    Create Jokebot.js

---
+++ Jokebot.js
@@ -0,0 +1,15 @@
+.hover:hover {
+  transform: scale(1.3);
+  transition: all .5s;
+  border-radius: 50px;
+  margin: 10px;
+  background-color: blue;
+  width: 350px;
+  height: 100px;
+}
+
+body{
+  background-color:red;
+}
+
+
